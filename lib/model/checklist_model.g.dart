fe8c538852fd28d4552ffd0726c0a3362d710ed5
amal-tech-dev <amalrofficial911@gmail.com>
2023-12-17 18:28:05 +0530

    removed color_controller

---
+++ lib/model/checklist_model.g.dart
@@ -0,0 +1,87 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'checklist_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ChecklistModelAdapter extends TypeAdapter<ChecklistModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ChecklistModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ChecklistModel(
+      title: fields[0] as String,
+      contentList: (fields[1] as List).cast<ContentModel>(),
+      dateTime: fields[2] as DateTime,
+      colorIndex: fields[3] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ChecklistModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.contentList)
+      ..writeByte(2)
+      ..write(obj.dateTime)
+      ..writeByte(3)
+      ..write(obj.colorIndex);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ChecklistModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ContentModelAdapter extends TypeAdapter<ContentModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  ContentModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ContentModel(
+      item: fields[0] as String,
+      check: fields[1] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ContentModel obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.item)
+      ..writeByte(1)
+      ..write(obj.check);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ContentModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
